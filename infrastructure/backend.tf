7d847937ada18235f0fcdc96385cf508b216546a
Jaylon Jones <147937281+Jaylon27@users.noreply.github.com>
2025-05-05 02:29:26 -0400

    Delete infrastructure/backend.tf

--- infrastructure/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "employeeonboardingsystem-resources"
-    storage_account_name = "employeesstorageaccount1"
-    container_name       = "prod-tfstate"
-    key                  = "prod.terraform.tfstate"
-  }
-}
